cf392f76c11d5e6cde5c4fbf5df77a3563bb61a1
noel paganelli <noel.paganelli@free.fr>
2017-06-26 14:40:33 +0200

    session with login/logout

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var session = require("express-session");
 var mongoose= require('mongoose');
 mongoose.connect('mongodb://noel:toto75@ds129342.mlab.com:29342/moviesapp' , function(err) {
   
@@ -7,7 +8,8 @@ mongoose.connect('mongodb://noel:toto75@ds129342.mlab.com:29342/moviesapp' , fun
 var movieSchema = mongoose.Schema({
     title: String,
     overview: String,
-    poster_path: String
+    poster_path: String,
+    user_id: String
 });
 var MovieModel = mongoose.model('movie', movieSchema);
 
@@ -21,8 +23,16 @@ var UserModel = mongoose.model('user', userSchema);
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(
+ session({ 
+  secret: 'a4f8071f-c873-4447-8ee2',
+  resave: false,
+  saveUninitialized: false,
+ })
+);
 
 app.get('/', function (req, res) {
+
   request("https://api.themoviedb.org/3/discover/movie?api_key=1ca44169216245030924859d77648835&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1", function(error, response, body) {
     body = JSON.parse(body);
     res.render('home', {movies: body.results, page: 'home'});
@@ -30,28 +40,33 @@ app.get('/', function (req, res) {
 });
 
 app.get('/review', function (req, res) {
-  console.log(req.query.id);
-  if(req.query.id != undefined) {
-    request("https://api.themoviedb.org/3/movie/"+req.query.id+"?api_key=1ca44169216245030924859d77648835&language=fr-FR", function(error, response, body) {
-      body = JSON.parse(body);
-      
-      var movie = new MovieModel ({
-        title: body.title,
-        overview: body.overview,
-        poster_path: body.poster_path
-      });
-      movie.save(function (error, movie) {
-        MovieModel.find(function (err, movies) {
-          res.render('home', {movies: movies, page: 'review'});
-        }) 
-      });
-    });
-  } else {
-    MovieModel.find(function (err, movies) {
-      res.render('home', {movies: movies, page: 'review'});
-    });
-  }
+  if(req.session.isLog  != true) {
+    res.redirect('/login');
+  } 
   
+  if(req.session.isLog  == true) {
+    if(req.query.id != undefined) {
+      request("https://api.themoviedb.org/3/movie/"+req.query.id+"?api_key=1ca44169216245030924859d77648835&language=fr-FR", function(error, response, body) {
+        body = JSON.parse(body);
+        
+        var movie = new MovieModel ({
+          title: body.title,
+          overview: body.overview,
+          poster_path: body.poster_path,
+          user_id : req.session.user_id
+        });
+        movie.save(function (error, movie) {
+          MovieModel.find( { user_id: req.session.user_id} , function (err, movies) {
+            res.render('home', {movies: movies, page: 'review'});
+          }) 
+        });
+      });
+    } else {
+      MovieModel.find({ user_id: req.session.user_id}, function (err, movies) {
+        res.render('home', {movies: movies, page: 'review'});
+      });
+    }
+  }
 });
 app.get('/contact', function (req, res) {
   res.render('contact');
@@ -62,6 +77,10 @@ app.get('/login', function (req, res) {
   if(req.query.email != undefined && req.query.password != undefined) {
     UserModel.findOne( { email: req.query.email, password: req.query.password} , function (err, user) {
       if(user != null) {
+        req.session.isLog    = true;
+        req.session.email    = req.query.email;
+        req.session.password = req.query.password;
+        req.session.user_id   = user.id;
         res.redirect('/');
       } else {
         res.render('login', {action : 'login', error: 'email ou mot de passe invalide'});
@@ -80,6 +99,10 @@ app.get('/signup', function (req, res) {
         password: req.query.password 
       });
       user.save(function (error, user) {
+        req.session.isLog    = true;
+        req.session.email    = req.query.email;
+        req.session.password = req.query.password;
+        req.session.userId   = user.id;
         res.redirect('/');
       });
    } else {
